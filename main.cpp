4fd5d24c7528fed24a0df9344a0bcb01f91bcfc5
rcdowning <111823343+rcdowning@users.noreply.github.com>
2022-10-02 20:06:26 +0000

    Final code running

--- main.cpp
+++ main.cpp
@@ -1,24 +1,22 @@
 /******************************************************************
-Description: This code will calculate the power in watts that is generated a specific water flow.
+Description: This code will calculate the power in watts that is generated a
+specific water flow.
 ******************************************************************/
-
-/******************************** Function Prototype ******************************************/
-//double powerGen(double, double)
-
-/****************************************** Main Code ****************************************/
-
 #include <iostream>
-
 using namespace std;
 
-int main() 
-{
+////////////////////////  Function Prototypes ////////////////////////
+double powerGen(double flow, double head);
+
+////////////////////////////// Main Code /////////////////////////////
+int main() {
   // variables
-  double P; // Power in watts
-  double Q; // volume of water flow m^3/s
-  double H; // Head of water in m
-  const double p = 1; // water density in kg/m^3
+  double Q;             // volume of water flow m^3/s
+  double H;              // Head of water in m
+  //double P;             // Power in watts
+  const double p = 1;   // water density in kg/m^3
   const double g = 9.8; // force of gravity in m/s^2
+
 
   // get user input for head distance and water flow
 
@@ -27,8 +25,20 @@ int main()
   cout << "Input the head distance [in m]: " << endl;
   cin >> H;
 
-  // calculate 
+  // call function
+  double power = powerGen(Q, H);
+  cout << "The power generated is " << power << " watts.";
 
-  P = ( p * Q * H * g );
-  cout << "P = " << P; 
+  // part b.) The test case of inputs Q = 20 and H = 30 = 5880 watts of power.
+  
 }
+/////////////////////////  User Defined Functions //////////////////////
+double powerGen(double flow, double head){
+  
+  const double p = 1;   // water density in kg/m^3
+  const double g = 9.8; // force of gravity in m/s^2
+
+  double P = (p * flow * head * g);
+  
+  return P;
+};
