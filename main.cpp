f5fc966462e79c9af0f3eaba00ba03bcffa41bf4
rcdowning <111823343+rcdowning@users.noreply.github.com>
2022-10-02 18:25:23 +0000

    Variable setup

---
+++ main.cpp
@@ -0,0 +1,27 @@
+/******************************************************************
+Description: This code will calculate the power in watts that is generated a specific water flow.
+******************************************************************/
+
+/******************************** Function Prototype ******************************************/
+//double powerGen(double, double)
+
+/****************************************** Main Code ****************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+  // variables
+  double P; // Power in watts
+  double Q; // volume of water flow m^3/s
+  double H; // Head of water in m
+  const double p = 1; // water density in kg/m^3
+  const double g = -9.8; // force of gravity in m/s^2
+
+  // get user input for head distance and water flow
+
+
+  
+}
